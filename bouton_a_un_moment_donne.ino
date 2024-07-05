88a19888b3743e85b13e2d85d14c6595b83d2b6a
Marc Robison <marcrobison@gmail.com>
2024-07-05 23:56:15 +0200

    removed comented code

--- bouton_a_un_moment_donne.ino
+++ bouton_a_un_moment_donne.ino
@@ -15,7 +15,6 @@ File uploadFile;
 const byte DNS_PORT = 53;
 DNSServer dnsServer;
 
-
 /* Set these to your desired credentials. */
 const char *ssid = APSSID;
 const char *password = APPSK;
@@ -52,7 +51,6 @@ void handleCaptivePortal() {
   server.sendContent(header);
   server.client().stop(); // End the current connection
 
-  // Change the state of the LED
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); // Toggle LED state
 }
 
@@ -99,28 +97,6 @@ void handleFileUpload() {
     LittleFS.end();
     Serial.println(String("Upload: END, Size: ") + upload.totalSize);
   }
-
-  // if (!fsOK) { return replyServerError(FPSTR(FS_INIT_ERROR)); }
-  // if (server.uri() != "/edit") { return; }
-  // HTTPUpload& upload = server.upload();
-  // if (upload.status == UPLOAD_FILE_START) {
-  //   String filename = upload.filename;
-  //   // Make sure paths always start with "/"
-  //   if (!filename.startsWith("/")) { filename = "/" + filename; }
-  //   DBG_OUTPUT_PORT.println(String("handleFileUpload Name: ") + filename);
-  //   uploadFile = fileSystem->open(filename, "w");
-  //   if (!uploadFile) { return replyServerError(F("CREATE FAILED")); }
-  //   DBG_OUTPUT_PORT.println(String("Upload: START, filename: ") + filename);
-  // } else if (upload.status == UPLOAD_FILE_WRITE) {
-  //   if (uploadFile) {
-  //     size_t bytesWritten = uploadFile.write(upload.buf, upload.currentSize);
-  //     if (bytesWritten != upload.currentSize) { return replyServerError(F("WRITE FAILED")); }
-  //   }
-  //   DBG_OUTPUT_PORT.println(String("Upload: WRITE, Bytes: ") + upload.currentSize);
-  // } else if (upload.status == UPLOAD_FILE_END) {
-  //   if (uploadFile) { uploadFile.close(); }
-  //   DBG_OUTPUT_PORT.println(String("Upload: END, Size: ") + upload.totalSize);
-  // }
 }
 
 void handleFileList() {
@@ -132,24 +108,13 @@ void handleFileList() {
   file_info += "<tr><th>File Name</th><th>Size (bytes)</th></tr>";
 
   while (dir.next()) {
-    // file_list += "<li>";
     String file_name = dir.fileName();
     size_t file_size = dir.fileSize();
     file_info += "<tr><td>" + file_name + "</td><td>" + String(file_size) + "</td></tr>";
-
-    // file_list += "</li>";
   }
-  // file_info += "</ul>";
-  
-  // String html = "<h1>Available files</h1>";
-  // html += file_info;
-  // html += "</body></html>";
-
   file_info += "</table>";
   file_info += "</body></html>";
 
-
-  // send last string
   server.send(200, "text/html", file_info);
 }
 
@@ -158,7 +123,7 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   Serial.begin(115200);
-  delay(5000);
+  delay(5000);  // allow serial port to be opened
 
   SPIFFSConfig cfg;
   cfg.setAutoFormat(false);
@@ -186,9 +151,7 @@ void setup() {
   IPAddress gateway(192,168,1,1);
   IPAddress subnet(255,255,255,0);
   WiFi.softAPConfig(local_IP, gateway, subnet);
-  
-  /* You can remove the password parameter if you want the AP to be open. */
- 
+   
   WiFi.softAP(ssid/*, password*/);
   Serial.print("\r\nConfiguring access point...\r\n");
 
